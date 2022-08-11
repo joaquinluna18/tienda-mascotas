8462e25cf9e88655022259839e282221a2fd5e90
joaquinluna18 <101607685+joaquinluna18@users.noreply.github.com>
2022-08-11 18:43:36 -0300

    ccambio en app.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <h1>Joaquin luna</h1>
-        <h2>tienda mascotas</h2>
+        <h2>tienda mascotas y mas</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
