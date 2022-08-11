3e001a8d8b266eef05dd5bcc5a1853321320033b
joaquinluna18 <101607685+joaquinluna18@users.noreply.github.com>
2022-08-11 18:28:29 -0300

    prueba

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Joaquin luna</h1>
+        <h2>tienda mascotas</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
