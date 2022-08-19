695d28a94463efbf3f719f017a7d1ff86cc9fdcf
joaquinluna18 <101607685+joaquinluna18@users.noreply.github.com>
2022-08-18 23:29:41 -0300

    logo dog

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,13 +2,17 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './Navbar.scss';
+import { SiDatadog} from "react-icons/si";
 
 function ColorSchemesExample() {
   return (
     <>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Koda Pet shop</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            Koda Pet shop
+            <SiDatadog></SiDatadog>
+            </Navbar.Brand>
           <Nav clasnpsName="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Products</Nav.Link>
