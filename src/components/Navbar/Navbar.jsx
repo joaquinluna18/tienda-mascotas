358899f97d27b425c6522dad566396f90095effd
joaquinluna18 <101607685+joaquinluna18@users.noreply.github.com>
2022-08-22 21:09:24 -0300

    Navbar con menu despegable

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,28 +1,35 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import './Navbar.scss';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import { SiDatadog} from "react-icons/si";
+import './Navbar.scss';
 
-function ColorSchemesExample() {
+
+function BasicExample() {
   return (
-    <>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">
-            Koda Pet shop
-            <SiDatadog></SiDatadog>
-            </Navbar.Brand>
-          <Nav clasnpsName="me-auto">
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">
+          <SiDatadog></SiDatadog>
+          Koda Pet Shop </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Products</Nav.Link>
-            <Nav.Link href="#pricing">About us</Nav.Link>
-            <Nav.Link href="#pricing">Contact</Nav.Link>
+            <Nav.Link href="#link">Contacto</Nav.Link>
+            <NavDropdown title="Productos" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Perros</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Gatos
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Otros</NavDropdown.Item>
+            </NavDropdown>
           </Nav>
-        </Container>
-      </Navbar>
-    </>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default ColorSchemesExample;
+export default BasicExample;
